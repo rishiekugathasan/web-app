beb9c6900e25c8c818664be04a6a574892c761ae
Aranesh <a.aranesh@gmail.com>
2021-09-11 15:43:58 -0400

    Update AltPanel.js

--- client/src/components/PanelsHomepage/whyYouSuceed/AltPanel.js
+++ client/src/components/PanelsHomepage/whyYouSuceed/AltPanel.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AltPanel() {
     
     const questions = [
-        {text:" It's a fact that coaches speed up your fitness success, but we dive further - we develop your mind muscle connection for you to become your own coach, for life."},
+        {text:" Fit for life - Our training will develop you to become your own coach, for life!"},
         {text: "From the very beginning you  are coached by experts in Movement, Strength, Medicine, and Nutrition. Our proven MyCure Team system will make you live healthier, stronger, happier."},
         {text: "You are beautifully unique - at MyCure fitness, your programs are custom tailored around your lifestyle. Realistic life can lead to extraordinary results."}
     ]
